78f1dd749bb97d55ddc0d936d11ca1efac33d80d
david <david@pop-os.localdomain>
2019-02-17 18:39:15 -0500

    add download file

---
+++ download_models.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget -P models http://download.tensorflow.org/models/deeplabv3_mnv2_cityscapes_train_2018_02_05.tar.gz
+wget -P models http://download.tensorflow.org/models/deeplabv3_cityscapes_train_2018_02_06.tar.gz
+wget -P models http://download.tensorflow.org/models/deeplab_cityscapes_xception71_trainfine_2018_09_08.tar.gz
+wget -P models http://download.tensorflow.org/models/deeplab_cityscapes_xception71_trainvalfine_2018_09_08.tar.gz
